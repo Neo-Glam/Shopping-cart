3e5b33c742008455bd73c8a5ff9766784b6f3950
gabriel <gabds.123@gmail.com>
2025-10-30 14:17:40 +0100

    add route to shop and cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,9 @@
+import { NavBar } from "./NavBar"
+
 export function Cart(){
     return(
         <>
+        <NavBar></NavBar>
         <p>this is the cart</p></>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 export function NavBar() {
   return (
-    <>
-    <Link to="/" >Home</Link>
-    <Link to="/Shop" >Shop</Link>
-    <Link to="/Cart" >Cart</Link>
-    </>
+    
+    <header>
+        <Link to="/" >Home</Link>
+        <Link to="/Shop" >Shop</Link>
+        <Link to="/Cart" >Cart</Link>
+    </header>
+    
   );
 }
