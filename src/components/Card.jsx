4e85bde2e53b24224220c8af062d30c142d35099
gabriel <gabds.123@gmail.com>
2025-10-30 14:52:45 +0100

    add placeholder style to card element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
+import "./Card.css"
+
 export function Card({name,image,price,bio}){
     return(
         <div className="Card">
             <img src={image} alt="" />
-            <h3>{name}</h3>
-            <h4>{price}</h4>
-            <h5>{bio}</h5>
+            <div className="info">
+                <h3>{name}</h3>
+                <h4>{price}</h4>
+                <h5>{bio}</h5>
+                <button>Add to Cart</button>
+            </div>
         </div>
     )
 }
